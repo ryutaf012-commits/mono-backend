3dbc90937d7f0c220ea4e146a77d377ba908af51
ryutaf012-commits <ryuta.f012@gmail.com>
2026-05-06 13:30:52 +0900

    fix render port

--- server.js
+++ server.js
@@ -246,7 +246,8 @@ app.delete("/api/categories/:id", (req, res) => {
 
 
 // 最後に起動
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
